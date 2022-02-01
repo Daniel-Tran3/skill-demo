7979267075ff8ce9936cd6d090a25c9e90a3332b
Daniel-Tran3 <daniel_tran@outlook.com>
2022-01-31 21:16:54 -0800

    Added lib and failing test

--- skill_demo_test.java
+++ skill_demo_test.java
@@ -1,1 +1,9 @@
-public class skill_demo_test {}
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skill_demo_test {
+    @Test
+    public void test_multiply() {
+        assertEquals(0, skill_demo.multiply(1, 2)); // This test will fail
+    }
+}
